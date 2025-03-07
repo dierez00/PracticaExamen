ba56a579d49b846153703783f07a367dc89b48ec
dierez00 <dierez00@gmail.com>
2025-03-06 23:33:48 -0600

    examen

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -21,5 +21,5 @@ export default function HomeScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
   title: { fontSize: 20, fontWeight: 'bold', marginBottom: 20 },
-  buttonContainer: { marginVertical: 5 }, // Agrega separación de 10px aprox entre botones
+  buttonContainer: { marginVertical: 5 }, 
 });
